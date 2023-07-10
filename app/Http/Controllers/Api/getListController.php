e98aa36da0b32210f7dbd80cda0cc64310c75e08
olamilly <olamyy.m@gmail.com>
2023-07-10 19:06:13 +0100

    paginate query

--- app/Http/Controllers/Api/getListController.php
+++ app/Http/Controllers/Api/getListController.php
@@ -8,7 +8,20 @@ use Illuminate\Http\Request;
 class getListController extends Controller
 {
     //read data from db and return json
-    public function read(){
-        return json_encode(firstModel::paginate(10),JSON_UNESCAPED_SLASHES);
+    public function read(Request $r){
+        if($r->query('take')){
+
+            //convert query to int
+            $take=$r->query('take');
+            settype($take, 'int');
+            
+            //perform comparison operation
+            if($take<=50)
+                return json_encode(firstModel::paginate($take),JSON_UNESCAPED_SLASHES);
+            else
+                return json_encode(firstModel::paginate(50),JSON_UNESCAPED_SLASHES); 
+        }
+        else
+            return json_encode(firstModel::paginate(10),JSON_UNESCAPED_SLASHES);
     }
 }
